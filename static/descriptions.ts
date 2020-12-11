e0dcfaecedcb2e43cd6e2d4a494afc93d1111d26
Timo Scheuermann <timo.scheuermann@gmx.de>
2020-12-11 09:28:25 +0100

    updated descriptions

--- static/descriptions.ts
+++ static/descriptions.ts
@@ -2,24 +2,32 @@ import { LesionClass, SLDescription } from "./interfaces";
 
 export const descriptions: Record<LesionClass, SLDescription> = {
   BCC: {
-    name: "",
-    short: "",
-    full: ""
+    name: "Basalzellkarzinom",
+    short:
+      "ist eine häufige Variante des epithelialen Hautkrebses, die selten metastasiert, aber unbehandelt zerstörerisch...",
+    full:
+      "ist eine häufige Variante des epithelialen Hautkrebses, die selten metastasiert, aber unbehandelt zerstörerisch wächst. Es tritt in verschiedenen morphologischen Varianten auf (flach, knötchenförmig, pigmentiert, zystisch)."
   },
   HEALTHY: {
-    name: "",
-    short: "",
-    full: ""
+    name: "Gesund",
+    short:
+      "Es konnte keine Läsion der Haut erkannt werden. Solltest du dir dennoch unsicher sein, kannst du Versuchen...",
+    full:
+      "Es konnte keine Läsion der Haut erkannt werden. Solltest du dir dennoch unsicher sein, kannst du Versuchen einen erneuten Scan der betroffenen Region durchführen. Hierzu ist ein anderer Winkel hilfreich."
   },
   MEL: {
-    name: "",
-    short: "",
-    full: ""
+    name: "Melanom",
+    short:
+      "ist ein bösartiges Neoplasma, das von Melanozyten abstammt und in verschiedenen Varianten auftreten...",
+    full:
+      "ist ein bösartiges Neoplasma, das von Melanozyten abstammt und in verschiedenen Varianten auftreten kann. Wenn es in einem frühen Stadium exzidiert wird, kann es durch einfache chirurgische Exzision geheilt werden. Melanome können invasiv oder nicht invasiv (insitu) sein."
   },
   AKIEC: {
-    name: "",
-    short: "",
-    full: ""
+    name: "Aktinische Keratosen",
+    short:
+      "und intrepithaeliale Karzinome sind häufige nicht-invasive Varianten des Plattenepithelkarzinoms...",
+    full:
+      "und intrepithaeliale Karzinome sind häufige nicht-invasive Varianten des Plattenepithelkarzinoms, die lo-kal und ohne Operation behandelt werden können. Da beide Typen durch UV-Licht induziert werden, ist die umgebende Haut in der Regel durch schwere Sonnenschäden gekennzeichnet."
   },
   BKL: {
     name: "Benign Keratosis",
@@ -29,19 +37,24 @@ export const descriptions: Record<LesionClass, SLDescription> = {
       "Die seborrhoische Keratose (Synonyme: Seborrhoische Warze, Alterswarze, Verruca seborrhoica, Basalzellpapillom) ist der häufigste gutartige Tumor der Haut. Er entwickelt sich meist in der zweiten Lebenshälfte, die Häufigkeit seines Auftretens nimmt mit steigendem Alter zu. Fast alle Menschen entwickeln im Laufe des Lebens eine bis mehrere seborrhoische Keratosen, wobei beide Geschlechter gleich häufig betroffen sind."
   },
   DF: {
-    name: "",
-    short: "",
-    full: ""
+    name: "Dermatofibrom",
+    short:
+      "ist eine gutartige Hautläsion, die entweder als gutartige Proliferation oder als Entzün-dungsreaktion auf ein...",
+    full:
+      "ist eine gutartige Hautläsion, die entweder als gutartige Proliferation oder als Entzün-dungsreaktion auf ein minimales Trauma betrachtet wird. Die häufigste dermatoskopi-sche Darstellung sind retikuläre Linien an der Peripherie mit einem zentralen weißen Fleck, der eine Fibrose zeigt."
   },
   NV: {
     name: "Melanocytic Nevi",
     short:
-      "eine begrenzte, gutartige Fehlbildung der Haut, die im Gegensatz zu anderen Arten von Nävi aus pigment...",
-    full: ""
+      "sind gutartige Neubildungen von Melanozyten und treten in einer Unzahl von Varianten auf. Die Varianten können...",
+    full:
+      "sind gutartige Neubildungen von Melanozyten und treten in einer Unzahl von Varianten auf. Die Varianten können sich aus dermatoksopischer Sicht deutlich unterschei-den. Im Gegensatz zum Melanom sind sie in der Regel symmetrisch in Bezug auf die Verteilung von Farbe und Struktur."
   },
   VASC: {
-    name: "",
-    short: "",
-    full: ""
+    name: "Vaskulären Hautläsion",
+    short:
+      "reichen im Datensatz von Kirschangiomen über Angiokeratome bis hin zu pyogenen Granulomen. Angiome sind dermatos...",
+    full:
+      "reichen im Datensatz von Kirschangiomen über Angiokeratome bis hin zu pyogenen Granulomen. Angiome sind dermatoskopisch durch eine rote oder violette Farbe und feste, gut umschriebene Strukturen gekennzeichnet, die als rote Klumpen oder Lakunen bekannt sind."
   }
 };
